993febff7bfae2605af32c32d4f150c9964ba803
epvelod <epvelod@hotmail.com>
2019-01-30 10:41:25 -0600

    fondo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,6 +12,7 @@ class Square extends React.Component {
 
     this.mouseover = this.mouseover.bind(this);
   }
+
   mouseover(e) {
     this.props.onMouseMove(e);
 
@@ -88,17 +89,56 @@ class Board extends React.Component {
     );
   }
 }
+class Titulo extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {currentCount:0};
 
+    this.timer = this.timer.bind(this);
+  }
+  
+  componentDidMount() {
+    var intervalId = setInterval(this.timer, 50);
+    // store intervalId in the state so it can be accessed later:
+    this.setState({intervalId: intervalId});
+  }
+
+  componentWillUnmount() {
+    // use intervalId from the state to clear the interval
+    clearInterval(this.state.intervalId);
+  }
+
+  timer() {
+    const children = this.props.children + '';
+    if(this.state.currentCount >= children.length){
+      this.setState({ currentCount: -9 });
+      clearInterval(this.state.intervalId);
+    }
+    else
+      this.setState({ currentCount: this.state.currentCount + 1 });
+  }
+
+  render() {
+    const children = this.props.children + '';
+    const title = this.state.currentCount>=0?children.substring(0, this.state.currentCount)+'_':children;
+
+    return (
+      <div className="ln-titulo">{title}</div>
+    );
+  }
+} 
 class Game extends React.Component {
   render() {
+    const Background = './images/background_image.jpg';
+    const backgroundImage = `url(${Background})`;
     return (
-      <div className="game">
+      <div className="game" 
+      style={{backgroundImage:backgroundImage,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'}}>
         <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <Titulo subttitulo="Se ve turbo vergas">Pruebas de react.</Titulo>
         </div>
       </div>
     );
